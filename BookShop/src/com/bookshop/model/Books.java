5ec4592bfe8fd643e5b50d32349cc5a8e0e0dc9f
Mybyb <1787901798@qq.com>
2020-05-01 14:14:57 +0800

    BYB的提交

--- BookShop/src/com/bookshop/model/Books.java
+++ BookShop/src/com/bookshop/model/Books.java
@@ -1,53 +1,71 @@
 package com.bookshop.model;
 
 public class Books {
-	private int bid;
-	private String bookname;
-	private Double b_price;
-	private String image;
-	private int stock;
-	public Books(int bid, String bookname, Double b_price, String image, int stock) {
+	private int id;
+	private String name;
+	private Double price;
+	private String img_url;
+	private int pnum;
+	private String category;
+	private String description;
+	
+	public Books(int id, String name, Double price, String img_url, int pnum, String category, String description) {
 		super();
-		this.bid = bid;
-		this.bookname = bookname;
-		this.b_price = b_price;
-		this.image = image;
-		this.stock = stock;
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.img_url = img_url;
+		this.pnum = pnum;
+		this.category = category;
+		this.description = description;
 	}
 	public Books() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public int getBid() {
-		return bid;
+	public int getId() {
+		return id;
 	}
-	public void setBid(int bid) {
-		this.bid = bid;
+	public void setId(int id) {
+		this.id = id;
 	}
-	public String getBookname() {
-		return bookname;
+	public String getName() {
+		return name;
 	}
-	public void setBookname(String bookname) {
-		this.bookname = bookname;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public Double getB_price() {
-		return b_price;
+	public Double getPrice() {
+		return price;
 	}
-	public void setB_price(Double b_price) {
-		this.b_price = b_price;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
-	public String getImage() {
-		return image;
+	public String getImg_url() {
+		return img_url;
 	}
-	public void setImage(String image) {
-		this.image = image;
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
 	}
-	public int getStock() {
-		return stock;
+	public int getPnum() {
+		return pnum;
 	}
-	public void setStock(int stock) {
-		this.stock = stock;
+	public void setPnum(int pnum) {
+		this.pnum = pnum;
 	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	
 	
 }
--- BookShop/src/com/bookshop/service/BooksService.java
+++ BookShop/src/com/bookshop/service/BooksService.java
@@ -1,5 +1,17 @@
 package com.bookshop.service;
 
-public class BooksService {
+import javax.annotation.Resource;
 
+import com.bookshop.dao.BooksDao;
+import com.bookshop.model.Books;
+
+public class BooksService {
+	@Resource
+	private BooksDao booksDao;
+	
+	public Books addBooks(){
+		
+		
+		return null;
+	}
 }
